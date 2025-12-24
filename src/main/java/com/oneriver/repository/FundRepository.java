e4a7622bca9101eac07f93e8b8b382d2e3a503a7
Furkan berkant kubat <berkant.kubat@fen.ktu.edu.tr>
2025-12-25 00:05:51 +0300

    refactor: clean up application.yaml and remove unused imports in FundIndexService and FundRepository

--- src/main/java/com/oneriver/repository/FundRepository.java
+++ src/main/java/com/oneriver/repository/FundRepository.java
@@ -2,13 +2,8 @@ package com.oneriver.repository;
 
 import com.oneriver.entity.Fund;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface FundRepository extends JpaRepository<Fund, Long> {
-
     List<Fund> findAllByFundCodeIn(List<String> fundCodes);
-
-    Optional<Fund> findByFundCode(String fundCode);
 }
